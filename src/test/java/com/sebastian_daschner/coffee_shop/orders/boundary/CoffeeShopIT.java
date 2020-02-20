9663073de2fe319df1141b2d76c7616969499c1a
Sebastian Daschner <git@sebastian-daschner.de>
2020-02-20 07:38:05 -0500

    updated Liberty plugin version

--- src/test/java/com/sebastian_daschner/coffee_shop/orders/boundary/CoffeeShopIT.java
+++ src/test/java/com/sebastian_daschner/coffee_shop/orders/boundary/CoffeeShopIT.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CoffeeShopIT {
+class CoffeeShopIT {
 
     private CoffeeShopSystem coffeeShopSystem = new CoffeeShopSystem();
 
